fb87e25745734ddcbb5029c16a9654d6d9c5358a
liudong <liudong@imaoding.com>
2017-10-12 15:10:50 +0800

    假设有两个线程，一个是线程 A，另一个是线程 B，两个线程分别依次打印 1-3 三个数字即可. 如果我们希望 B 在 A 全部打印 完后再开始打印呢？我们可以利用 thread.join() 方法

--- src/ThreadTest/threadJoin/ThreadJoin.java
+++ src/ThreadTest/threadJoin/ThreadJoin.java
@@ -4,10 +4,12 @@ package ThreadTest.threadJoin;
  * Created by Dong_Liu
  * date：2017/10/12
  * 假设有两个线程，一个是线程 A，另一个是线程 B，两个线程分别依次打印 1-3 三个数字即可。我们来看下代码：
- * 如果我们希望 B 在 A 全部打印 完后再开始打印呢？我们可以利用 thread.join() 方法，代码如下:
  */
 public class ThreadJoin {
     private static void demo2() {
+        /**
+         * 如果我们希望 B 在 A 全部打印 完后再开始打印呢？我们可以利用 thread.join() 方法，代码如下:
+         * */
         Thread A = new Thread(new Runnable() {
             @Override
             public void run() {
