257b81a848df66bcbd839b5375f2132453d88401
aidon0818 <30116992@qq.com>
2018-09-04 17:40:43 +0800

    形参里面出现，表示的是可变参数

---
+++ src/JavaTest/Test1.java
@@ -0,0 +1,15 @@
+package JavaTest;
+
+public class Test1 {
+
+    public static void sumArr(int... arr) {
+        for (int i : arr) {
+            System.out.print(i + "+");
+        }
+    }
+
+    public static void main(String[] args) {
+//        sumArr(2); //在这里传入一个数字，一串数字，一个数字数组都可以，很方便
+        sumArr(1, 2, 3);
+    }
+}
